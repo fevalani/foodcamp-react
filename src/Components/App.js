6d4aec94749793f53f104f28dce417928957b419
Fernando Valani Cavalcante <valanife@gmail.com>
2021-07-08 12:17:42 -0300

    imagens readme

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import Top from "./Top";
 import Container from "./Container";
-//import Confirmation from "./Confirmation";
 
 export default function App() {
   return (
--- src/Components/ConfirmButton.js
+++ src/Components/ConfirmButton.js
@@ -5,11 +5,46 @@ export default function ConfirmButton({
   drinkCounter,
   dessertCounter,
 }) {
+  function foodString(foodCounter, drinkCounter, dessertCounter) {
+    let price = 0;
+    let foodText = "";
+    let drinkText = "";
+    let dessertText = "";
+
+    foodCounter.forEach((item) => {
+      price = price + item.amount * parseFloat(item.price.replace(",", "."));
+
+      foodText =
+        foodText + "\n" + (item.title + " " + "(" + item.amount + "x)");
+    });
+
+    drinkCounter.forEach((item) => {
+      price = price + item.amount * parseFloat(item.price.replace(",", "."));
+
+      drinkText =
+        drinkText + "\n" + (item.title + " " + "(" + item.amount + "x)");
+    });
+
+    dessertCounter.forEach((item) => {
+      price = price + item.amount * parseFloat(item.price.replace(",", "."));
+
+      dessertText =
+        dessertText + "\n" + (item.title + " " + "(" + item.amount + "x)");
+    });
+
+    let whatsAppMessageLink = `
+          Olá, gostaria de fazer o pedido: \n- Prato: ${foodText}\n- Bebida: ${drinkText}\n- Sobremesa: ${dessertText}\nTotal: R$ ${price.toFixed(
+      2
+    )}
+      `;
+    return whatsAppMessageLink;
+  }
+
   return (
     <div className="bottom-bar">
       <a
         href={
-          "https://wa.me/5524992494516?text=" +
+          "https://wa.me/5521999999999?text=" +
           encodeURIComponent(
             foodString(foodCounter, drinkCounter, dessertCounter)
           )
@@ -23,37 +58,4 @@ export default function ConfirmButton({
       </a>
     </div>
   );
-}
-
-function foodString(foodCounter, drinkCounter, dessertCounter) {
-  let price = 0;
-  let foodText = "";
-  foodCounter.forEach((item) => {
-    price = price + item.amount * parseFloat(item.price.replace(",", "."));
-
-    foodText = foodText + "\n" + (item.title + " " + "(" + item.amount + "x)");
-  });
-
-  let drinkText = "";
-  drinkCounter.forEach((item) => {
-    price = price + item.amount * parseFloat(item.price.replace(",", "."));
-
-    drinkText =
-      drinkText + "\n" + (item.title + " " + "(" + item.amount + "x)");
-  });
-
-  let dessertText = "";
-  dessertCounter.forEach((item) => {
-    price = price + item.amount * parseFloat(item.price.replace(",", "."));
-
-    dessertText =
-      dessertText + "\n" + (item.title + " " + "(" + item.amount + "x)");
-  });
-
-  let whatsAppMessageLink = `
-        Olá, gostaria de fazer o pedido: \n- Prato: ${foodText}\n- Bebida: ${drinkText}\n- Sobremesa: ${dessertText}\nTotal: R$ ${price.toFixed(
-    2
-  )}
-    `;
-  return whatsAppMessageLink;
 }
